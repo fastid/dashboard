bb335ea005910b65ee797e3a40c7a497a58a8d24
kostya <kostya@yandex.ru>
2023-10-15 23:51:52 +0300

    refactor code

--- src/states/Config.ts
+++ src/states/Config.ts
@@ -1,6 +1,5 @@
-import {atom, selector} from "recoil";
-import {api, InterfacesAPI} from "../api/API";
-import {set} from "react-hook-form";
+import {atom} from "recoil";
+import {InterfacesAPI} from "../api/API";
 
 export const ConfigState = atom<InterfacesAPI.Config>({
   key: 'Config',
@@ -12,25 +11,3 @@ export const ConfigState = atom<InterfacesAPI.Config>({
     recaptcha_site_key: null,
   },
 })
-
-export const ConfigSelector = selector({
-  key: 'ConfigSelector',
-  get: ({get}) => {
-    const config = get(ConfigState)
-
-    api.GetConfig().then(response => {
-      config.is_init = true
-      config.is_setup = response.data.is_setup
-      config.captcha = response.data.captcha
-      config.captcha_usage = response.data.captcha_usage
-      config.recaptcha_site_key = response.data.recaptcha_site_key
-
-    }).catch(error=> {
-
-    })
-    return config;
-  },
-  set: ({set}, newVal) => {
-    set(ConfigState, newVal)
-  }
-});
